f66d5300a19818c12132bb4729be785eec9c5a92
Ken Nersisyan <karapet.nersisyan@clerkie.io>
2024-01-13 13:20:23 -0800

    inital commit

---
+++ src/index.ts
@@ -0,0 +1,20 @@
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import cookieParser from 'cookie-parser';
+import compression from 'compression';
+import cors from 'cors';
+
+const app = express();
+const PORT = process.env.PORT || 8000;
+
+app.use(cors({ credentials: true }));
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
+
+const server = http.createServer(app);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
