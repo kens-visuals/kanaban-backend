8dd37ee04f1d0a488193a261010472605ed4cbcf
Ken Nersisyan <karapet.nersisyan@clerkie.io>
2024-06-21 12:24:01 -0700

    fix url

--- src/router/TaskRouter.ts
+++ src/router/TaskRouter.ts
@@ -13,7 +13,7 @@ const taskRouter: Router = express.Router();
 taskRouter.post('/task/:user_id', createTask);
 taskRouter.put('/task/:user_id/:task_id', editTask);
 taskRouter.delete('/task/:user_id/:task_id', deleteTask);
-taskRouter.get('/task/:user_id/:parent_column_id/:task_id', getTaskById);
+taskRouter.get('/task/:user_id/:parent_board_id/:task_id', getTaskById);
 taskRouter.get('/task/:user_id/:parent_board_id', getTasksByParentBoardId);
 
 export default taskRouter;
